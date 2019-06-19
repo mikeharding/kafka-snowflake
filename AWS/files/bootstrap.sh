dda0de69a9a7582bbf4d64666fe5128864937261
Mike Harding <mharding@gmail.com>
2019-06-19 08:37:49 -0700

    Upgraded Confluent version to 5.2

--- AWS/files/bootstrap.sh
+++ AWS/files/bootstrap.sh
@@ -9,27 +9,42 @@ sudo rpm -Uvh jdk-8u211-linux-x64.rpm
 export JAVA_HOME=/usr/java/jdk1.8.0_211-amd64
 export PATH=$PATH:/usr/java/jdk1.8.0_211-amd64/bin
 
-sudo rpm --import https://packages.confluent.io/rpm/4.0/archive.key
-
+#sudo rpm --import https://packages.confluent.io/rpm/4.0/archive.key
+sudo rpm --import https://packages.confluent.io/rpm/5.2/archive.key
 
 sudo echo "[Confluent.dist]
 name=Confluent repository (dist)
-baseurl=https://packages.confluent.io/rpm/4.0/7
+baseurl=https://packages.confluent.io/rpm/5.2/7
 gpgcheck=1
-gpgkey=https://packages.confluent.io/rpm/4.0/archive.key
+gpgkey=https://packages.confluent.io/rpm/5.2/archive.key
 enabled=1
 
 [Confluent]
 name=Confluent repository
-baseurl=https://packages.confluent.io/rpm/4.0
+baseurl=https://packages.confluent.io/rpm/5.2
 gpgcheck=1
-gpgkey=https://packages.confluent.io/rpm/4.0/archive.key
+gpgkey=https://packages.confluent.io/rpm/5.2/archive.key
 enabled=1" > confluent.repo
+
+
+#sudo echo "[Confluent.dist]
+#name=Confluent repository (dist)
+#baseurl=https://packages.confluent.io/rpm/4.0/7
+#gpgcheck=1
+#gpgkey=https://packages.confluent.io/rpm/4.0/archive.key
+#enabled=1
+
+#[Confluent]
+#name=Confluent repository
+#baseurl=https://packages.confluent.io/rpm/4.0
+#gpgcheck=1
+#gpgkey=https://packages.confluent.io/rpm/4.0/archive.key
+#enabled=1" > confluent.repo
 
 sudo mv confluent.repo /etc/yum.repos.d/
 sudo yum clean all
 
-sudo yum -y install confluent-platform-oss-2.11
+sudo yum -y install confluent-community-2.12
 
 wget http://central.maven.org/maven2/com/snowflake/snowflake-kafka-connector/0.3/snowflake-kafka-connector-0.3.jar
 sudo mkdir /usr/share/java/kafka-connect-snowflake
@@ -50,7 +65,7 @@ echo "for _ in range(100):" >> gen-data.py
 echo "    print(str(datetime.datetime.now()) + ',' + fake.name() + ',' + fake.ipv4_private())" >> gen-data.py
 
 touch read-topic.sh
-echo "kafka-avro-console-consumer --bootstrap-server localhost:9092 --topic connect-test --from-beginning" > read-topic.sh
+echo "sudo kafka-avro-console-consumer --bootstrap-server localhost:9092 --topic demotopic --from-beginning" > read-topic.sh
 sudo chmod +x read-topic.sh
 
 
